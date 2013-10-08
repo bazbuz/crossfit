a822bd6efe693a58f3abd01274e997a7e4347b4b
Barry Burrows <barryburrows7@hotmail.com>
2013-10-08 10:08:48 +0100

    connection to database

--- app.js
+++ app.js
@@ -1,73 +1,171 @@
+/*
+//  Codename: Bangers
+//      Direct Sales Channel for 'Cheap' New Cars
+*/
 
-/**
- * Module dependencies.
- */
+/*
+//  Module Dependencies
+//      For further information on these and any other packages, you can 
+//      simply head to npmjs.org and look up the package - for example, for 
+//      underscore, go to https://npmjs.org/package/underscore
+*/
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var http                = require('http'),
+    path                = require('path');
 
-var config ={
-	dbDatabase:"crossfit",
-	dbHostname:"ds031968.mongolab.com",
-	dbPort:31968,
-	dbUsername:"barry",
-	dbPassword:"joseph"
-};
+var express             = require('express');
 
-var mongo = require('mongodb');
+//  TODO:   Switch to using Swig instead of Jade at some point. Not urgent.
+
+var mongo               = require('mongodb');
+
+var routes              = require('./routes');
+
+/*
+//  Configuration
+//      See http://s.phuu.net/12PFa6J by Tom Ashworth (@phuunet)
+*/
+
+var configFile;
+try {
+    configFile = require('./config.json');
+}
+catch (err) {
+    console.log(err);
+    configFile = {};
+}
+
+//  Configure by using process environment variables first, falling back to the 
+//  configuration file (config.json) if it exists, finally falling back to 
+//  sensible defaults for local development environments.
+
+var config = {
+    "port":   
+                    configFile.port ||
+                    5000,
+
+    "dbHostname": 
+                    configFile.dbHostname ||
+                    "localhost",
+    "dbPort":       process.env.DB_PORT ||
+                    configFile.dbPort ||
+                    27017,
+    "dbDatabase":  
+                    configFile.dbDatabase ||
+                    "test",
+    "dbUsername":  
+                    configFile.dbUsername ||
+                    "",
+    "dbPassword":   
+                    configFile.dbPassword ||
+                    ""
+}
+
+console.log(config);
+
+/*
+//  Database Configuration
+*/
 
 var db = new mongo.Db(
-	config.dbDatabase,
-	new mongo.Server(
-		config.dbHostname,
-		config.dbPort,
-		{
-			autoreconnect:true
-		}),
-		{
-			w:0
-		}
-	);
-db.open(function(openErr,openData){
-	if(openData){
-		openData.authenticate(
-			config.dbUsername,
-			config.dbPassword,
-			function(authErr,authData){
-				console.log("Database:connected");
-			});
-	}
+    config.dbDatabase,
+    new mongo.Server(
+        config.dbHostname,
+        config.dbPort,
+        {
+            auto_reconnect: true
+        }
+    ),
+    /* No write acknowledgement needed just yet - we're not writing anything (are we?) */
+    {
+        w: 0
+    }
+);
+
+//  Open the Database
+db.open(function (openErr, openData) {
+
+    if (openData) {
+        openData.authenticate(
+            config.dbUsername, 
+            config.dbPassword,
+            function (authErr, authData) {
+                if (authData) {
+                    console.log("Database: Connected.");
+                }
+                else {
+                    console.log(authErr);
+                }
+            }
+        );
+    }
+    else {
+        console.log(openErr);
+    }
 });
 
-var crossfit = db.collection("crossit");
+//  The whole thing falls over if vehicles is var'd here, which makes me think 
+//  that this does not belong here and is possibly being called out of scope.
+wods = db.collection("wods");
+
+
+/*
+//  Express Configuration
+*/
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
+//  All Environments
+app.set("port", config.port);
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+
+app.use(express.logger("dev"));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.favicon());
+// app.use(express.cookieParser(/* 'some secret string goes here' */));
+// app.use(express.cookieSession());
+// app.use(express.compress()); // Compresses responses with gzip/deflate
+// app.use(express.csrf()); // Cross Site Request Forgery Protection
+    
+//  Express Routing
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+
+
+//  Development-only Configuration
+if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+}
+else {
+    app.use(function errorHandler (err, req, res, next) {
+        res.status(500);       
+        res.render(
+            'error', 
+            { 
+                error: err 
+            }
+        );
+    });
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+/*
+//  Routing
+*/
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+routes(app);
+
+/*
+//  Start the HTTP Server
+*/
+
+http
+    .createServer(app)
+    .listen(
+        app.get('port'), 
+        function () {
+            console.log("Express server listening on port " + app.get('port'));
+        }
+    );
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
-
 /*
- * GET home page.
- */
+//  Routes Loader
+//      /routes/index.js
+*/
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Crossfit' });
-};
-exports.results = function(req,res) {
-	console.log('results page');
-};
+//  Require access to the filesystem
+var	fs = require('fs');
+
+module.exports = function (app) {
+    
+    fs.readdirSync(__dirname).forEach(function (file) {
+        // Don't re-load index.js or any file that isn't a .js file
+        if (file === 'index.js' || file.substr(file.lastIndexOf('.') + 1) !== 'js') 
+            return;
+        var name = file.substr(0, file.indexOf('.'));
+
+        // Require the individual route files
+        require('./' + name)(app);
+    });
+
+}
